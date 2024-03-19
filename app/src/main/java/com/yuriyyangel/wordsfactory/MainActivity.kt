7bf3b996ffe41d4345f46db500d20899b56dc093
Yuriy Yangel <arlosaneq@gmail.com>
2024-03-19 18:41:16 +0700

    Initial commit

---
+++ app/src/main/java/com/yuriyyangel/wordsfactory/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.yuriyyangel.wordsfactory
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yuriyyangel.wordsfactory.ui.theme.WordsFactoryTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Greeting(name = "Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WordsFactoryTheme {
+        Greeting("Android")
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
+    alias(libs.plugins.androidLibrary) apply false
+}
